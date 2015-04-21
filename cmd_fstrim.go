5ba8cf85e02814c68c711ace22c5173ca4bc52bd
Vasiliy Tolstov <v.tolstov@selfip.ru>
2015-04-21 10:09:06 +0300

    fix trim
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- cmd_fstrim.go
+++ cmd_fstrim.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"encoding/json"
-	"os"
-	"unsafe"
-
-	"github.com/vtolstov/go-ioctl"
+	"os/exec"
 )
 
 var cmdFstrim = &Command{
@@ -17,15 +14,20 @@ func init() {
 	commands = append(commands, cmdFstrim)
 }
 
+// TODO: USE NATIVE SYSCALL
 func fnFstrim(d map[string]interface{}) interface{} {
-	var r int
-
+	//	r := ioctl.FsTrimRange{Start: 0, Length: -1, MinLength: 0}
 	id, _ := (d["id"].(json.Number)).Int64()
-
-	if f, err := os.Open("/dev/sda1"); err == nil {
-		defer f.Close()
-		ioctl.Fitrim(f.Fd(), uintptr(unsafe.Pointer(&r)))
-	}
+	/*
+		if f, err := os.OpenFile("/", os.O_RDONLY, os.FileMode(0400)); err == nil {
+			defer f.Close()
+			err = ioctl.Fitrim(uintptr(f.Fd()), uintptr(unsafe.Pointer(&r)))
+			if err != nil {
+	*/
+	exec.Command("fstrim", "/").Run()
+	//			log.Printf("guest-fstrim: %s", err.Error())
+	//		}
+	//	}
 
 	return &Response{
 		Return: id,

fc048f93a3755eff34f3456b3fe590b44eb9ee4a
Vasiliy Tolstov <v.tolstov@selfip.ru>
2015-11-03 15:51:51 +0000

    fix
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

---
+++ doc_agent_update.go
@@ -0,0 +1,12 @@
+/*
+
+guest-agent-update.
+
+Example:
+	{ "execute": "guest-agent-update", "arguments": {
+		"path": string // required, http, https, file path to qemu-ga binary for update
+		}
+	}
+
+*/
+package main // import "github.com/vtolstov/qemu-ga"
--- doc_exec.go
+++ doc_exec.go
@@ -2,13 +2,13 @@
 
 guest-exec command have two versions.
 
-First:
+First only used in own solutions:
 	{ "execute": "guest-exec", "arguments": {
 		"command": string // required, base64 encoded command name to execute with args including newline
 		}
 	}
 
-Second:
+Second from official qemu-ga:
 	{ "execute": "guest-exec", "arguments": {
 		"path": string, // required, command name to execute
 		"arg": string, // optional, arguments to executed command

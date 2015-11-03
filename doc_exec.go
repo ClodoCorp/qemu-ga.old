2b437fe1255d7a855f389da71d69219343f4ec1e
Vasiliy Tolstov <v.tolstov@selfip.ru>
2015-11-03 15:47:34 +0000

    fix
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- doc_exec.go
+++ doc_exec.go
@@ -4,17 +4,17 @@ guest-exec command have two versions.
 
 First:
 	{ "execute": "guest-exec", "arguments": {
-		"command": string
+		"command": string // required, base64 encoded command name to execute with args including newline
 		}
 	}
 
 Second:
 	{ "execute": "guest-exec", "arguments": {
-		"path": string,
-		"arg": string,
-		"env": string,
-		"input": string,
-		"capture-output": bool
+		"path": string, // required, command name to execute
+		"arg": string, // optional, arguments to executed command
+		"env": string, // optional, environment to executed command
+		"input": string, // optional, base64 encoded string
+		"capture-output": bool // optional, capture stdout/stderr
 		}
 	}
 

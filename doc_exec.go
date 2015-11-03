8c3b324ac26eaaf4c8e8d04d403de5969a4d0d3b
Vasiliy Tolstov <v.tolstov@selfip.ru>
2015-11-03 15:38:40 +0000

    fix
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- doc_exec.go
+++ doc_exec.go
@@ -1,18 +1,18 @@
 /*
 
-   {"execute": "guest-exec", "arguments": {
-       "command": string
-     }
-   }
+	{ "execute": "guest-exec", "arguments": {
+		"command": string
+		}
+	}
 
-   {"execute": "guest-exec", "arguments": {
-       "path": string,
-       "arg": string,
-       "env": string,
-       "input": string,
-       "capture-output": bool
-     }
-   }
+	{ "execute": "guest-exec", "arguments": {
+		"path": string,
+		"arg": string,
+		"env": string,
+		"input": string,
+		"capture-output": bool
+		}
+	}
 
 */
 package main // import "github.com/vtolstov/qemu-ga"
